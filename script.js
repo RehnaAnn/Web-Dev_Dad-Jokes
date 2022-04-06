c5ac6b83875cf6bc83ecd0ad122c6675cb02cadb
RehnaAnn <rehna.anthru@gmail.com>
2022-04-06 13:42:10 -0400

    Committing files to Dev_Dad Jokes

---
+++ script.js
@@ -0,0 +1,35 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn=document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+generateJoke()
+
+//using async
+async function generateJoke(){
+  const config = {
+    headers: {
+      Accept : 'application/json',
+    },
+  }
+
+const res= await fetch('https://icanhazdadjoke.com', config)
+const data = await res.json()
+  jokeEl.innerHTML=data.joke
+}
+
+/*
+using then
+function generateJoke(){
+  const config = {
+    headers: {
+      Accept : 'Application/json',
+    },
+  }
+
+fetch('https://icanhazdadjoke.com', config)
+.then((response) => response.json())
+.then((data) => {
+  jokeEle.innerHTML=data.joke
+})
+}
+*/
